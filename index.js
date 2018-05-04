5aec944e412410926e093cdf7539339dda520c3a
Sykander <sgul@spartaglobal.com>
2018-05-04 08:31:50 +0100

    added functionality to the score display such that it changes color when you cannot shoot a fireball

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ for (var i = 0; i < board.length; i++) {
     board[i][j] = 0;
   }
 }
+// global variables
+// current score
+var score_count = 0;
+// player life
+var player_alive=true;
+// current chance of a comet spawning
+var chance = 0.1;
+// current difficulty in terms of speed adjustment
+var difficulty = 0;
+// current status of reset
+var reset= false;
+// current fireball recharge status
+var fireball_recharge = 0;
+// current player input status (allowed or paused)
+var paused = false;
+// first load check
+var first_load = true;
 
+// function to reset the game back to the initial state
 function resetFunction(){
+  // kill the current player if they're still alive
+  player_alive = false;
+
   // reset game Array
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board.length; j++) {
@@ -16,11 +37,20 @@ function resetFunction(){
   }
   // reset game board
   drawToPage();
+  // new score
   score_count = 0;
+  // player life
+  player_alive=true;
+  // new chance of a comet spawning
   chance = 0.1;
+  // new difficulty in terms of speed adjustment
   difficulty = 0;
-  paused = false;
+  // new status of reset
+  reset= false;
+  // new fireball recharge status
   fireball_recharge = 0;
+  // disables the pausing of player input
+  paused = false;
 
   // place 10 in the board to represent the player
   board[9][5].id = '10';
@@ -28,21 +58,35 @@ function resetFunction(){
   var table = document.getElementById('game-frame').children[0].children;
   var row = table[9].children;
   row[5].id = 'player';
-  // reset page
-  document.getElementById('message').innerHTML = '';
-  document.getElementById('menu').innerHTML = '';
-  reset = true;
-}
 
-// global variables
-var score_count = 0, player_alive=true,chance = 0.1, difficulty = 0, reset= false, paused = false, fireball_recharge = 0, recharge_time = 50;
+  // update score_display
+  document.getElementById('score_display').innerHTML = '';
+  document.getElementById('reset').innerHTML = '';
+  document.getElementById('new_game').innerHTML = 'START';
+  document.getElementById('message').innerHTML = '';
+  document.getElementById('menu').innerHTML = 'MENU';
+  // mark reset as completed
+  reset = true;
+
+  if(first_load==true){
+    first_load = false;
+    newGame();
+  }
+}
 
 // new game
 function newGame(){
   // start a new game only if the game has just been reset
   if(reset == true){
+    // adjust score display
+    document.getElementById('score_display').innerHTML = 0;
+    document.getElementById('reset').innerHTML = 'RESET';
+    document.getElementById('new_game').innerHTML = '';
+    document.getElementById('message').innerHTML = '';
+    document.getElementById('menu').innerHTML = '';
+
+    // new life
     player_alive = true;
-    recharge_time = 50;
     gameLoop();
     reset = false;
   }
@@ -50,15 +94,20 @@ function newGame(){
 
 // game loop - this runs repeatedly
 function gameLoop(){
+  // stop the loop is player dead
   if(player_alive == true){
+    // pull values from the page board
     drawToArray();
+    // check if the player has died since the last cycle
     checkForDeath();
+    // update the score and score display
     updateScore();
+    // raise any fireballs up the screen
     raiseFireballs();
     // small chance of having a complete wall of comets
     // this will automatically refill your fireball recharge so you have a chance to make it through
     // if you had already used your fireball and it was recharging
-    if(0.01> Math.random()){
+    if(chance/20 > Math.random()){
       // spawn in new blocks
       for (var i = 0; i < board.length; i++) {
         board[0][i] = 1;
@@ -67,68 +116,8 @@ function gameLoop(){
     }
     dropBlocks();
     drawToPage();
-    incrementDificulty();
-    setTimeout(gameLoop, 200 - difficulty*100);
-  }
-}
-
-// increments difficulty whilst never surpassing 1
-function incrementDificulty(){
-  difficulty = difficulty + (1-difficulty)/2000;
-  chance = chance + (0.5-chance)/2000;
-}
-
-// function to check if the player is about to die
-function checkForDeath(){
-  // get player coordinates
-  var player = document.getElementById('player')
-  if(player){
-    // do nothing
-  }
-  else {
-    // end game
-    document.getElementById('message').innerHTML = 'YOU DIED';
-    document.getElementById('menu').innerHTML = 'MENU';
-    player_alive = false;
-  }
-}
-
-// function to update the score
-function updateScore(){
-  // get new score
-  for (var i = 0; i < board.length; i++) {
-    if (board[9][i] == 1) {
-      // increase the score
-      score_count++;
-      // if the fireball function needs recharging then recharge it here
-      if(fireball_recharge>0){
-        fireball_recharge--;
-      }
-    }
-  }
-
-  // display the new score
-  var score_display = document.getElementById('score_display');
-  score_display.innerHTML = score_count;
-}
-
-// function to raise fireballs up the screen
-function raiseFireballs(){
-  // clear top row
-  for (var i = 0; i < board.length; i++) {
-    if (board[0][i] == 2) {
-      board[0][i] = 0;
-    }
-  }
-  // raise each fireball
-  for (var i = 1; i < board.length ; i++) {
-    for (var j = 0; j < board.length; j++) {
-      if (board[i][j]==2) {
-        board[i-1][j] = 2;
-        board[i][j] = 0;
-        drawToPage();
-      }
-    }
+    incrementDifficulty();
+    setTimeout(gameLoop, 230 - difficulty*60);
   }
 }
 
@@ -161,45 +150,66 @@ function drawToArray(){
   }
 }
 
-// draws the board array to the page
-function drawToPage(){
-  // get table information
-  var table = document.getElementById('game-frame').children[0].children;
+// function to check if the player is about to die
+function checkForDeath(){
+  // get player coordinates
+  var player = document.getElementById('player')
+  if(player){
+    // do nothing
+  }
+  else {
+    // end game
+    document.getElementById('message').innerHTML = 'YOU DIED';
+    document.getElementById('menu').innerHTML = 'MENU';
+    player_alive = false;
+  }
+}
 
-  // clear the page
+// function to update the score
+function updateScore(){
+  // catch the score_display DOM object
+  var score_display = document.getElementById('score_display');
+
+  // get new score
   for (var i = 0; i < board.length; i++) {
-    var row = table[i].children;
-    for (var j = 0; j < row.length; j++) {
-      row[j].classList.remove('block');
-      row[j].classList.remove('fireball');
-      row[j].id = '';
+    if (board[9][i] == 1) {
+      // increase the score
+      score_count++;
+      // if the fireball function needs recharging then recharge it here
+      if(fireball_recharge>0){
+        score_display.style.color ='red';
+        fireball_recharge--;
+      }
+      if (fireball_recharge == 0) {
+        score_display.style.color='#4d94ff';
+      }
     }
   }
+  // display the new score
+  score_display.innerHTML = score_count;
+}
 
-  // draw to the page
-  // for all rows
+// function to raise fireballs up the screen
+function raiseFireballs(){
+  // clear top row
   for (var i = 0; i < board.length; i++) {
-    var row = table[i].children;
-    // for all columns
-    for (var j = 0; j < row.length; j++) {
-      // if it contains a block
-      if(board[i][j] == 1){
-        row[j].classList.add('block');
-      }
-      // if it contains a fireball
-      if (board[i][j] == 2) {
-        row[j].classList.add('fireball');
-      }
-      // if it contains the player
-      if (board[i][j] == 10) {
-        row[j].id = 'player';
+    if (board[0][i] == 2) {
+      board[0][i] = 0;
+    }
+  }
+  // raise each fireball
+  for (var i = 1; i < board.length ; i++) {
+    for (var j = 0; j < board.length; j++) {
+      if (board[i][j]==2) {
+        board[i-1][j] = 2;
+        board[i][j] = 0;
+        drawToPage();
       }
     }
   }
 }
 
-// drop blocks function
-// drops the blocks on the page
+// drops the blocks on the top of the page
 function dropBlocks(){
   // clear bottom row
   for (var i = 0; i < board.length; i++) {
@@ -222,7 +232,6 @@ function dropBlocks(){
       }
     }
   }
-
   // spawn in new blocks
   for (var i = 0; i < board.length; i++) {
     var n = Math.random();
@@ -232,22 +241,70 @@ function dropBlocks(){
   }
 }
 
+// draws the board array to the page
+function drawToPage(){
+  // get table information
+  var table = document.getElementById('game-frame').children[0].children;
+
+  // clear the page
+  for (var i = 0; i < board.length; i++) {
+    var row = table[i].children;
+    for (var j = 0; j < row.length; j++) {
+      row[j].classList.remove('block');
+      row[j].classList.remove('fireball');
+      row[j].id = '';
+    }
+  }
+  // draw to the page
+  // for all rows
+  for (var i = 0; i < board.length; i++) {
+    var row = table[i].children;
+    // for all columns
+    for (var j = 0; j < row.length; j++) {
+      // if it contains a block
+      if(board[i][j] == 1){
+        row[j].classList.add('block');
+      }
+      // if it contains a fireball
+      if (board[i][j] == 2) {
+        row[j].classList.add('fireball');
+      }
+      // if it contains the player
+      if (board[i][j] == 10) {
+        row[j].id = 'player';
+      }
+    }
+  }
+}
+
+// increments difficulty and chance
+function  incrementDifficulty(){
+  // this determines how fast the game gets more difficulty
+  var difficulty_rate = Math.random()*1000;
+  // increment equations
+  difficulty = difficulty + (1-difficulty)/difficulty_rate;
+  // this determines the upper limit on the values of chance
+  var chance_max = 0.2;
+  chance = chance + (chance_max-chance)/difficulty_rate;
+}
+
 // function to add an event listener to the whole page for key presses
 var addEvent = document.addEventListener ? function(target,type,action){
     if(target){
-        target.addEventListener(type, action, false);
-    }
+      target.addEventListener(type, action, false);
+  }
 } : function(target,type,action){
     if(target){
-        target.attachEvent('on' + type, action, false);
+      target.attachEvent('on' + type, action, false);
     }
 }
 
 // add functionality for each key pressed (key down and key up)
 addEvent(document,'keydown',function(e) {
-    // grab the event
-    e = e || window.event;
-    var key = e.which || e.keyCode;
+  // grab the event
+  e = e || window.event;
+  var key = e.which || e.keyCode;
+  if(paused == false){
     // switch case for which key was pressed
     switch (key) {
       // case for W
@@ -265,11 +322,11 @@ addEvent(document,'keydown',function(e) {
         var col = row[x];
 
         // if you can move into the square then do so
-        if(col.classList == '' && paused == false){
+        if(col.classList == ''){
           player.id = '';
           col.id = 'player';
         }
-        break;
+      break;
       // case for A
       case 65:
         // getting player coordinates and adjusting them
@@ -285,87 +342,91 @@ addEvent(document,'keydown',function(e) {
         var col = row[x];
 
         // if you can move into the square then do so
-        if(col.classList == '' && paused ==false){
+        if(col.classList == ''){
           player.id = '';
           col.id = 'player';
         }
         break;
-      // case for S
-      case 83:
-        // getting player coordinates and adjusting them
-        var player = document.getElementById('player');
-        var x = player.cellIndex;
-        var y = player.parentElement.rowIndex;
-        y++;
-        if(y>=9) y = 9;
-
-        // get new coordinates
-        var table = document.getElementById('game-frame').children;
-        var row = table[0].children[y].children;
-        var col = row[x];
-
-        // if you can move into the square then do so
-        if(col.classList == '' && paused == false){
-          player.id = '';
-          col.id = 'player';
-        }
-      break;
-      // case for D
-      case 68:
-        // getting player coordinates and adjusting them
-        var player = document.getElementById('player');
-        var x = player.cellIndex;
-        var y = player.parentElement.rowIndex;
-        x++;
-        if(x>=9) x = 9;
-
-        // get new coordinates
-        var table = document.getElementById('game-frame').children;
-        var row = table[0].children[y].children;
-        var col = row[x];
-
-        // if you can move into the square then do so
-        if(col.classList == '' && paused == false){
-          player.id = '';
-          col.id = 'player';
-        }
-      break;
-      // case for space
-      case 32:
-      // if the player isn't shooting a fireball, is alive and has recharged their fireball timer
-        if(paused == false && player_alive == true && fireball_recharge == 0){
+        // case for S
+        case 83:
+          // getting player coordinates and adjusting them
           var player = document.getElementById('player');
           var x = player.cellIndex;
           var y = player.parentElement.rowIndex;
+          y++;
+          if(y>=9) y = 9;
 
-          // adjust coordinates and set movement to paused
-          y = y-1;
-          paused = true;
-          setTimeout(unpause, 100);
-
-          // function to unpause the pause on movement
-          function unpause(){
-            paused = false;
-          }
-
-          // get the cell above the player
+          // get new coordinates
           var table = document.getElementById('game-frame').children;
           var row = table[0].children[y].children;
           var col = row[x];
 
-          // function which shoots the fireball
-          shootFireball();
-          // function to shoot fireball
-          function shootFireball(){
-            if(col.classList == ''){
-              col.classList.add('fireball');
-              board[y][x] = 2;
-              // set the fireball recharge to 20
-              fireball_recharge= recharge_time;
+          // if you can move into the square then do so
+          if(col.classList == ''){
+            player.id = '';
+            col.id = 'player';
+          }
+        break;
+        // case for D
+        case 68:
+          // getting player coordinates and adjusting them
+          var player = document.getElementById('player');
+          var x = player.cellIndex;
+          var y = player.parentElement.rowIndex;
+          x++;
+          if(x>=9) x = 9;
+
+          // get new coordinates
+          var table = document.getElementById('game-frame').children;
+          var row = table[0].children[y].children;
+          var col = row[x];
+
+          // if you can move into the square then do so
+          if(col.classList == ''){
+            player.id = '';
+            col.id = 'player';
+          }
+        break;
+        // case for space
+        case 32:
+          // if the player isn't shooting a fireball, is alive and has recharged their fireball timer
+          if(player_alive == true && fireball_recharge == 0){
+            var player = document.getElementById('player');
+            var x = player.cellIndex;
+            var y = player.parentElement.rowIndex;
+
+            // adjust coordinates and set movement to paused
+            y = y-1;
+            paused = true;
+            setTimeout(unpause, 100);
+
+            // function to unpause the pause on movement
+            function unpause(){
+              paused = false;
+            }
+
+            // get the cell above the player
+            var table = document.getElementById('game-frame').children;
+            var row = table[0].children[y].children;
+            var col = row[x];
+
+            // function which shoots the fireball
+            shootFireball();
+            // function to shoot fireball
+            function shootFireball(){
+              // this determines how many points to recharge the fireball
+              var recharge_time = 50
+
+              if(col.classList == ''){
+                col.classList.add('fireball');
+                board[y][x] = 2;
+                // set the fireball recharge to 20
+                fireball_recharge= recharge_time;
+              }
             }
           }
-        }
-      break;
+        break;
+      }
     }
   }
 )
@@ -376,7 +437,7 @@ function playTheme(){
   audio.play();
 }
 
-// run the game
+// run the reset on load
 resetFunction();
-newGame();
+// play the theme on load
 playTheme();
